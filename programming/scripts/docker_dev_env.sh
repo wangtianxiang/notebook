46e0495a86b1c109b9321e908881d9569b0f0409
wangtianxiang <mail.txwang@qq.com>
2023-04-12 19:19:21 +0800

    Do not share .gitconfig

--- programming/scripts/docker_dev_env.sh
+++ programming/scripts/docker_dev_env.sh
@@ -95,11 +95,10 @@ main(){
         # Generate shared folder if folder not exist
         mkdir -p ${HOME}/work
         mkdir -p ${HOME}/.ssh
-        [[ -f ${HOME}/.gitconfig ]] || touch ${HOME}/.gitconfig
 
         docker_run_cmd="docker run --gpus all --cap-add=SYS_ADMIN --name $container_name \
             --hostname $container_name  -v ${HOME}/work:${HOME}/work \
-            -v ${HOME}/.gitconfig:${HOME}/.gitconfig -v ${HOME}/.ssh:${HOME}/.ssh"
+            -v ${HOME}/.ssh:${HOME}/.ssh"
 
         if [ -n "$port" ]
         then
